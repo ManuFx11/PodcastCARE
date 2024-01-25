c6231161de5ae71a3656a6bc7e074a73d8245f03
Manu Paez <manu9lin@gmail.com>
2024-01-25 18:24:42 +0100

    Instalacion conector vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output : 'server',
+  output: 'server',
   image: {
     remotePatterns: [{
       protocol: "https"
     }]
   },
-  integrations: [react()]
+  integrations: [react()],
+  adapter: vercel()
 });
