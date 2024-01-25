c78ac97563539aa372532817187f34219f1eb7b5
Manu Paez <manu9lin@gmail.com>
2024-01-25 19:00:51 +0100

    borrado vercel

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/care-experts_5thyGQhL.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/care-experts_FP5lZn9-.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/sobre-el-club_lw3zVnqi.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/sobre-el-club_1d3fCJ9x.mjs');
-
-export { page };
