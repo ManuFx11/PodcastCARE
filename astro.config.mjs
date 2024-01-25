cf7c687db12a67f87f8748960ab5396740ae4632
Manu Paez <manu9lin@gmail.com>
2024-01-25 18:39:15 +0100

    Config 2 vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
-import vercelServerless from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   image: {
     remotePatterns: [{
       protocol: "https"
     }]
   },
   integrations: [react()],
-  adapter: vercelServerless({
+  adapter: vercel({
     edgeMiddleware: true,
   })
 });
