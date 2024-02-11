3a07907d55e50efd12f27e23ed51edd134b11dcb
Manu Paez <manu9lin@gmail.com>
2024-02-11 18:27:16 +0100

    Avance Home

---
+++ src/utilities/recursos-import.js
@@ -0,0 +1,4 @@
+export const icono1 = require('../../public/recursos/icono1.png');
+export const icono2 = require('../../public/recursos/icono2.png');
+export const icono3 = require('../../public/recursos/icono3.png');
+export const icono4 = require('../../public/recursos/icono4.png');
